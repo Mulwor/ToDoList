53623b3abf351f70d2b0461046691f40f0bd8ffe
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-12-04 14:40:13 +0300

    refactor: delete the rendering of the scond todolist

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,11 @@ function App() {
         { id: 5, title: "WesternJS", isDone: false },
         { id: 6, title: "Zeva", isDone: true}
     ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am your father", isDone: false },
-        { id: 3, title: "Luke, i'm alive", isDone: true },
-        { id: 4, title: "Luke, i'm sleeping", isDone: false },
-        { id: 5, title: "Luke, shut up", isDone: true }
-    ]
 
 
     return (
         <>
             <TodoList title = "What to learn" tasks={tasks1}/>
-            <TodoList title = "Who are you" tasks={tasks2}/>
         </>
     );
 }
