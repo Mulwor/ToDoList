f83bb712cff973dbd998139b09162dcae37cc638
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-12-04 15:39:26 +0300

    feat: hook useState for removeTask by click

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TodoList from "./components/TodoList";
 
 function App() {
-    let tasks1 = [
+    let [tasks, setTasks] = useState([
         { id: 1, title: "HTML&CSS", isDone: false },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
         { id: 4, title: "SolidJS", isDone: true },
         { id: 5, title: "WesternJS", isDone: false },
         { id: 6, title: "Zeva", isDone: true},
-    ]
+    ])
 
     // Для удаления по клику на х
     function removeTask(id: number) {
-        tasks1 = tasks1.filter(task => task.id !== id)
-        console.log(tasks1)
+        let removeTasks = tasks.filter(task => task.id !== id)
+        setTasks(removeTasks)
     }
 
 
     return (
         <>
             <TodoList title = "What to learn"
-                      tasks={tasks1}
+                      tasks={tasks}
                       removeTask = {removeTask}
             />
         </>
